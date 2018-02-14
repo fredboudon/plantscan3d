8531f1cd0288fb0ba7c6b11fb956c9c9cabd1f53
Frederic Boudon <frederic.boudon@cirad.fr>
2018-02-14 10:46:21 +0100

    fix pb with renaming

--- src/openalea/plantscan3d/shareddata.py
+++ src/openalea/plantscan3d/shareddata.py
@@ -3,7 +3,7 @@ from os.path import join as pj
 def get_shared_data(file, share_path='data'):
     try:
         from openalea.deploy.shared_data import get_shared_data_path
-        import vplants.treeeditor3d as ted
+        import openalea.plantscan3d as ted
         shared_data_path = get_shared_data_path(ted.__path__, share_path=share_path)
         return pj(shared_data_path, file)
     except:
